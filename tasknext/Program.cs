0cbe07a2234ff682b6fac230474a72192970c8e6
Hmuriy Monakh <max_08931993@mail.ru>
2022-08-05 21:06:24 +0300

    masive

---
+++ tasknext/Program.cs
@@ -0,0 +1,35 @@
+﻿void PrintArray(int[] col)
+    {
+        int count = col.Length;
+        int position = 0;
+        while (position < count)
+            {
+                Console.Write($"{col[position]} ");
+                position++;
+            }
+    }
+
+Console.WriteLine("Enter the massive size ");
+
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] massive =new int[size];
+int sumN = 0;
+int sumP = 0;
+
+for(int i = 0; i < massive.Length; i++)
+    {
+        massive[i] = new Random().Next(-9,10);
+
+    }
+
+PrintArray(massive);
+for(int i = 0; i < massive.Length; i++) massive[i] *= -1;
+
+
+
+System.Console.WriteLine();
+Console.WriteLine();
+
+
+
