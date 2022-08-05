692d0624e2e119a40c5bad3b2f03941d1ffa4926
Hmuriy Monakh <max_08931993@mail.ru>
2022-08-05 22:02:13 +0300

    masive6

--- taskpogramm/Program.cs
+++ taskpogramm/Program.cs
@@ -71,13 +71,81 @@ static void PrintArray(int[] col)
 }
 */
 
-//Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+//Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+//Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+//Результат запишите в новом массиве.
 //[1 2 3 4 5] -> 5 8 3
 //[6 7 3 6] -> 36 21
 
+/* сам попробую
+Console.WriteLine("Ведите массив ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] massive =new int[size];
+for(int i = 0; i < size; i++);
+{
+    
+}
 
+*/
+// массив задачи от алисы последняя задача
+/*Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
 
+int[] massive = new int[size];
+int[] massive_new = new int[size/2];
 
+for(int i = 0; i < size; i++)
+    {
+        massive[i] = new Random().Next(0,5);
+     
+    }
+PrintArray(massive);
+     for(int i = 0; i < size/2; i++)
+        {
+            massive_new[i] = massive[i]*massive[size-i-1]; 
+        }
+    
+    void PrintArray(int[] col)
+    {
+        int count = col.Length;
+        int position = 0;
+        while (position < count)
+            {
+                Console.Write($"{col[position]} ");
+                position++;
+            }
+    }
+
+PrintArray(massive_new);
+*/
+
+int[] list = new int[13];
+string result = String.Empty;
+
+for (int i = 0; i < list.Length; i++)
+{
+    list[i] = new Random().Next(1, 20);
+}
+
+void PrintArray(int[] col)
+    {
+        int count = col.Length;
+        int position = 0;
+        while (position < count)
+            {
+                Console.Write($"{col[position]} ");
+                position++;
+            }
+    }
+PrintArray(list);
+Console.WriteLine();
+for (int i = 0; i < list.Length / 2; i++)
+{
+    result += $"{list[i] * list[list.Length - i - 1]}, ";
+}
+if (list.Length % 2 != 0) result += $"{list[list.Length / 2]}";
+
+Console.WriteLine(result);
 
 
 
